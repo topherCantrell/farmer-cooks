f3c2f7d741458a867be7389be87e2a6d4d5ab5be
Christopher Cantrell <topherCantrell@gmail.com>
2020-02-24 10:59:29 -0600

    General get/drop

--- src/app_console_test.py
+++ src/app_console_test.py
@@ -7,26 +7,60 @@ engine = ConsoleEngine()
 engine.ROOMS = {
     
     'default' : {
-        'objects' : {},
+        'objects' : [],
         'messages' : {
+            "miscNoWay"          : "<genMessNoWay> You can't go that direction.",
+            "miscHandEmpty"      : "<genMessHandEmpty> Your hand is empty.",
+            "miscNothingToGet"   : "<genMessNothingThere> There is nothing to get.",
+            "miscHandFull"       : "<genMessHandFull> You already have something in that hand.",
+            "miscLeftHand"       : "<genMessLeftHand> In your left hand: ",
+            "miscRightHand"      : "<genMessRightHand> In your right hand: ",
+            "miscOK"             : "<genMessOK> OK.",
+            "miscNothingHappens" : "<genMessNothingHappens> Nothing happens.",
             "miscDontUnderstand" : [ 
-               "<genMessWhat1> I don't understand.",
-               "<genMessWhat2> What?",
-               "<genMessWhat3> Hmmmmmm."
-            ],
-            "miscNoWay" : "<genMessNoWay> You can't go that direction.",
+                                   "<genMessWhat1> I don't understand.",
+                                   "<genMessWhat2> What?",
+                                   "<genMessWhat3> Hmmmmmm."
+                                   ]
         },
         'commands' : [
-            "north", "say miscNoWay",
-            "south", "say miscNoWay",
-            "east",  "say miscNoWay",
-            "west",  "say miscNoWay",
+            'north', 'say miscNoWay',
+            'south', 'say miscNoWay',
+            'east',  'say miscNoWay',
+            'west',  'say miscNoWay',
+            
+            # Look
+            "look" ,      "generalDescribeRoom",     # LOOK describe the room
+            
+            # Gets                        
+            "get * - *" , "say miscNothingToGet",    # GET left/right with nothing to get (doesn't matter what's in the hand)
+            "get * * -" , "generalGet",              # GET left/right something with nothing in hand (generic GET handler)
+            "get * * *" , "say miscHandFull",        # GET left/right something but something in hand        
+            
+            # Uses
+            "use * -",    "say miscHandEmpty",       # USE left/right with nothing in hand
+            "use * *",    "say miscNothingHappens",  # Use left/right with something in hand
+            
+            # Drops
+            "drop * -",   "say miscHandEmpty",       # DROP left/right with nothing in hand
+            "drop * *",   "generalDrop",             # DROP left/right with something in hand            
         ]
     },
     
     'RedRoom' : {
         'description' : '<descRed> This is the red room. You can go south.',
-        'objects' : {},
+        'objects' : [
+            {
+                'id'    :  'trumpet',
+                'short' : '<objTrumpetShort> trumpet',
+                'long'  : '<objTrumpetLong> There is a silver trumpet here.'
+            },
+            {
+                'id'    :  'lamp',
+                'short' : '<objLampShort> lamp',
+                'long'  : '<objLampLong> There is a brass lamp here.'
+            }
+        ],
         'commands' : [
             'south', 'goto BlueRoom'
         ]
@@ -34,7 +68,7 @@ engine.ROOMS = {
     
     'BlueRoom' : {
         'description' : '<descBlue> This is the blue room. You can go north.',
-        'objects' : {},
+        'objects' : [],
         'commands' : [
             'north', 'goto RedRoom'
         ]
